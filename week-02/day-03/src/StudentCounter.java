b54394d700b1d75e7e89bc7fd056592e0cd5c4d2
tomdreidel <redly.tamas@gmail.com>
2017-09-13 21:09:47 +0200

    corrected ArrayList declaration, changed variable name

--- week-02/day-03/src/StudentCounter.java
+++ week-02/day-03/src/StudentCounter.java
@@ -2,41 +2,41 @@ import java.util.*;
 
 public class StudentCounter{
     public static void main(String... args){
-        ArrayList<Map<String, Object>> map = new ArrayList<Map<String, Object>>();
+        ArrayList<Map<String, Object>> map = new ArrayList<>();
 
         // Given this list of hashmaps...
 
-        Map<String, Object> row0 = new HashMap<String, Object>();
+        Map<String, Object> row0 = new HashMap<>();
         row0.put("name", "Rezso");
         row0.put("age", 9.5);
         row0.put("candies", 2);
         map.add( row0 );
 
-        Map<String, Object> row1 = new HashMap<String, Object>();
+        Map<String, Object> row1 = new HashMap<>();
         row1.put("name", "Gerzson");
         row1.put("age", 10);
         row1.put("candies", 1);
         map.add( row1 );
 
-        Map<String, Object> row2 = new HashMap<String, Object>();
+        Map<String, Object> row2 = new HashMap<>();
         row2.put("name", "Aurel");
         row2.put("age", 7);
         row2.put("candies", 3);
         map.add( row2 );
 
-        Map<String, Object> row3 = new HashMap<String, Object>();
+        Map<String, Object> row3 = new HashMap<>();
         row3.put("name", "Zsombor");
         row3.put("age", 12);
         row3.put("candies", 5);
         map.add( row3 );
 
-        Map<String, Object> row4 = new HashMap<String, Object>();
+        Map<String, Object> row4 = new HashMap<>();
         row4.put("name", "Olaf");
         row4.put("age", 12);
         row4.put("candies", 7);
         map.add( row4 );
 
-        Map<String, Object> row5 = new HashMap<String, Object>();
+        Map<String, Object> row5 = new HashMap<>();
         row5.put("name", "Teodor");
         row5.put("age", 3);
         row5.put("candies", 2);
@@ -47,7 +47,7 @@ public class StudentCounter{
         //  - Sum the age of people who have lass than 5 candies
 
         ArrayList<String> candyKings = new ArrayList<>();
-        float ageSummer = 0;
+        double ageAggregator = 0;
 
         for (int i = 0; i < map.size(); i++) {
             if ((int) map.get(i).get("candies") > 4) {
@@ -57,7 +57,7 @@ public class StudentCounter{
 
         for (int i = 0; i < map.size(); i++) {
             if ((int) map.get(i).get("candies") < 5) {
-               ageSummer += Double.parseDouble(String.valueOf(map.get(i).get("age")));
+               ageAggregator += Double.parseDouble(String.valueOf(map.get(i).get("age")));
             }
         }
 
@@ -66,7 +66,6 @@ public class StudentCounter{
         System.out.println(candyKings);
         System.out.println("--------------------------------");
         System.out.println("Sum of age of people with less than 5 candies:");
-        System.out.println(ageSummer);
-
+        System.out.println(ageAggregator);
     }
 }
