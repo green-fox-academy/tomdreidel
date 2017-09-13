7dd7a4554b22f97a321fe392a35dea747a0ee753
tomdreidel <redly.tamas@gmail.com>
2017-09-13 23:44:49 +0200

    minor change in function

--- week-02/day-02/src/AppendAnimals.java
+++ week-02/day-02/src/AppendAnimals.java
@@ -12,6 +12,6 @@ public class AppendAnimals {
     }
 
     public static String appender(String nimalsToCorrect) {
-       return nimalsToCorrect = nimalsToCorrect + "a";
+       return nimalsToCorrect + "a";
     }
 }
