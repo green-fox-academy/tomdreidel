0fb970d2e8a8cffec170ce7e39cc8cb284535119
tomdreidel <redly.tamas@gmail.com>
2017-09-13 20:44:44 +0200

    corrected spacing in ArrayLists

--- week-02/day-03/src/Matchmaking.java
+++ week-02/day-03/src/Matchmaking.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Matchmaking{
     public static void main(String... args){
-        ArrayList<String> girls = new ArrayList<>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
-        ArrayList<String> boys = new ArrayList<>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+        ArrayList<String> girls = new ArrayList<>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+        ArrayList<String> boys = new ArrayList<>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
         ArrayList<String> order = new ArrayList<>();
 
         // Join the two lists by matching one girl with one boy in the order list
