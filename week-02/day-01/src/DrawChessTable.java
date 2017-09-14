4d1262777ca7890e99e5dc5b6757b90e11a664bf
tomdreidel <redly.tamas@gmail.com>
2017-09-14 14:00:58 +0200

    every excercise is done

---
+++ week-02/day-01/src/DrawChessTable.java
@@ -0,0 +1,25 @@
+public class DrawChessTable {
+
+  public static void main(String[] args) {
+    // Crate a program that draws a chess table like this
+    //
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    // % % % %
+    //  % % % %
+    //
+
+    for (int i = 0; i < 8; i++) {
+      if (i % 2 == 0) {
+        System.out.println("% % % %");
+      }
+      else {
+        System.out.println(" % % % %");
+      }
+    }
+  }
+}
