311788c8b48c59e3fcc755e8a1f8195aa940c099
tomdreidel <redly.tamas@gmail.com>
2017-09-14 12:55:34 +0200

    done with multiple if statements

--- week-02/day-01/src/DrawDiagonal.java
+++ week-02/day-01/src/DrawDiagonal.java
@@ -15,24 +15,34 @@ public class DrawDiagonal {
         // The square should have as many lines as the number was
 
         Scanner numberScanner = new Scanner(System.in);
-        System.out.println("Give me the size of the square and I will draw a diagonal to it:");
+        System.out.println("Give me the size of the square with a diagonal in it:");
         int size = numberScanner.nextInt();
 
         if (size < 1) {
-            System.out.println("Not big enough");
-        } else if (size == 1) {
+            System.out.println("Not big enough to draw");
+        }
+        else if (size == 1) {
             System.out.println("%%%%%");
-        } else if (size == 2) {
+        }
+        else if (size == 2) {
             System.out.println("%%%%%");
             System.out.println("%%%%%");
-        } else if (size == 3) {
-            System.out.println("%%%%%");
-            System.out.println("% % %");
-            System.out.println("%%%%%");
-        } else {
+        }
+        else {
             System.out.println("%%%%%");
             for (int i = 0; i < (size - 2); i++) {
-                System.out.println("%   %");
+                if (i == 0 && i == (size - 3)) {
+                    System.out.println("% % %");
+                }
+                else if (i == 0) {
+                    System.out.println("%%  %");
+                }
+                else if (i == (size - 3)) {
+                    System.out.println("%  %%");
+                }
+                else {
+                    System.out.println("% % %");
+                }
             }
             System.out.println("%%%%%");
         }
