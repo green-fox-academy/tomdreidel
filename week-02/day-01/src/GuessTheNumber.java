c580e3e68cc92d4ff005dd53b82153beeae13fde
tomdreidel <redly.tamas@gmail.com>
2017-09-14 13:18:23 +0200

    clearance increased

--- week-02/day-01/src/GuessTheNumber.java
+++ week-02/day-01/src/GuessTheNumber.java
@@ -18,7 +18,7 @@ public class GuessTheNumber {
     System.out.println("Enter a number to guess:");
     storedNumber = guessScanner.nextInt();
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 40; i++) {
       System.out.println();
     }
 
