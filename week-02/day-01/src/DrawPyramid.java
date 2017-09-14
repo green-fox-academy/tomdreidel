aaf508cbd386e019a568496faf09b0190951fb0d
tomdreidel <redly.tamas@gmail.com>
2017-09-14 10:42:22 +0200

    modified the for loops, now it is working

--- week-02/day-01/src/DrawPyramid.java
+++ week-02/day-01/src/DrawPyramid.java
@@ -17,11 +17,11 @@ public class DrawPyramid {
         System.out.println("Give me the size of the pyramid:");
         int number = numberScanner.nextInt();
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
             for (int j = 0; j < (number - i); j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k <= i; k++) {
+            for (int k = 1; k <= (2 * i - 1) ; k++) {
                 System.out.print("*");
             }
             System.out.println();
