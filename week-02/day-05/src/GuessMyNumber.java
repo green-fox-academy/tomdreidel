87e7d573a53d2c6ebd2d700db4d1bcf2caeb980d
tomdreidel <redly.tamas@gmail.com>
2017-09-15 11:42:36 +0200

    custom number range added

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -10,33 +10,45 @@ public class GuessMyNumber {
     //  Make the range customizable (ask for it before starting the guessing).
     //  You can add lives. (optional)
 
-    int number = ((int) (Math.random() * 100 -1) + 1);
-
     Scanner guessScanner = new Scanner(System.in);
+
+    System.out.println("Give me the lower bound of numbers:");
+    int lowerBound = guessScanner.nextInt();
+    System.out.println("Give me the upper bound of numbers:");
+    int upperBound = guessScanner.nextInt();
+
+    int number = ((int) (Math.random() * upperBound - lowerBound) + lowerBound);
+
     System.out.println("Guess my number! You have 5 tries.");
     System.out.println("Take a guess:");
     int guess = guessScanner.nextInt();
+    int lives = 5;
 
-    while (guess != number) {
+    while (lives > 0) {
       for (int i = 0; i < 4; i++) {
         if (guess == number) {
           System.out.println("Congrats! You won!");
           break;
         }
         else if (guess < number) {
+          lives -= 1;
           System.out.println("Too small. Guess again:");
           System.out.println("You have " + (4 - i) + " " + ((i == 3) ? "try" : "tries") + " left");
           guess = guessScanner.nextInt();
         }
         else {
+          lives -= 1;
           System.out.println("Too big. Guess again:");
           System.out.println("You have " + (4 - i) + " " + ((i == 3) ? "try" : "tries") + " left");
           guess = guessScanner.nextInt();
         }
       }
-      System.out.println("You lost!");
-      System.out.println("It was: " + number);
-      break;
+      if (lives == 1) {
+        System.out.println("You lost!");
+        System.out.println("It was: " + number);
+        break;
+      }
+    break;
     }
   }
 }
