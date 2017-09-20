d2cd6c49d706f578e0a50b4099a69270ec8579bd
tomdreidel <redly.tamas@gmail.com>
2017-09-20 16:23:21 +0200

    similar but not ready

--- week-03/day-03/src/EnvelopeStar.java
+++ week-03/day-03/src/EnvelopeStar.java
@@ -10,9 +10,35 @@ public class EnvelopeStar {
       // reproduce this:
       // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
 
+      int lineStep = 10;
 
+      // quarter 1
 
+      for (int i = 0; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(25 + i * lineStep, HEIGHT / 2, WIDTH / 2, HEIGHT / 2 - i * lineStep);
+      }
 
+      // quarter 2
+
+      for (int i = 0; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(WIDTH / 2, 25 + i * lineStep, WIDTH / 2 + i * lineStep, HEIGHT / 2);
+      }
+
+      // quarter 3
+
+      for (int i = 1; i < HEIGHT / 2 / lineStep - 3; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(25 + i * lineStep, HEIGHT / 2, WIDTH / 2, HEIGHT / 2 + i * lineStep);
+      }
+
+      // quarter 4
+
+      for (int i = 1; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(WIDTH / 2 + lineStep + i * lineStep, HEIGHT / 2, WIDTH / 2, HEIGHT - 25 - i * lineStep);
+      }
     }
 
 //    Don't touch the code below
