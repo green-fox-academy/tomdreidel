d1a985d724f13f25dfc80542407db7acfddf1054
tomdreidel <redly.tamas@gmail.com>
2017-09-20 15:44:40 +0200

    working with a bunch of for loops

--- week-03/day-03/src/LinePlayQuarters.java
+++ week-03/day-03/src/LinePlayQuarters.java
@@ -11,8 +11,51 @@ public class LinePlayQuarters {
       // and repeat this pattern in each quarter:
       // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
+      int lineStep = 10;
 
+      // quarter 1
 
+      for (int i = 1; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(0, 25 + i * lineStep, i * lineStep, HEIGHT / 2);
+      }
+      for (int i = 1; i < HEIGHT / 2 / lineStep - 3; i++) {
+        graphics.setColor(new Color(176, 36, 233));
+        graphics.drawLine(25 + i * lineStep, 0, WIDTH / 2, i * lineStep);
+      }
+
+      // quarter 2
+
+      for (int i = 1; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(WIDTH / 2, 25 + i * lineStep, HEIGHT / 2 + i * lineStep, HEIGHT / 2);
+      }
+      for (int i = 1; i < HEIGHT / 2 / lineStep - 3; i++) {
+        graphics.setColor(new Color(176, 36, 233));
+        graphics.drawLine(WIDTH / 2 + 25 + i * lineStep, 0, WIDTH, i * lineStep);
+      }
+
+      // quarter 3
+
+      for (int i = 1; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(0, HEIGHT / 2 + 25 + i * lineStep, i * lineStep, HEIGHT);
+      }
+      for (int i = 1; i < HEIGHT / 2 / lineStep - 3; i++) {
+        graphics.setColor(new Color(176, 36, 233));
+        graphics.drawLine(25 + i * lineStep, HEIGHT / 2, WIDTH / 2, HEIGHT / 2 + i * lineStep);
+      }
+
+      // quarter 4
+
+      for (int i = 1; i < WIDTH / 2 / lineStep - 1; i++) {
+        graphics.setColor(new Color(52,214,	105));
+        graphics.drawLine(WIDTH / 2, HEIGHT / 2 + 25 + i * lineStep, WIDTH / 2 + i * lineStep, HEIGHT);
+      }
+      for (int i = 1; i < HEIGHT / 2 / lineStep - 3; i++) {
+        graphics.setColor(new Color(176, 36, 233));
+        graphics.drawLine(WIDTH / 2 + 25 + i * lineStep, HEIGHT / 2, WIDTH, HEIGHT / 2 + i * lineStep);
+      }
     }
 
 //    Don't touch the code below
