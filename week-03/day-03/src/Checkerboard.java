1927064d585fc11210eb86172874360a9171f7dc
tomdreidel <redly.tamas@gmail.com>
2017-09-20 14:01:48 +0200

    implemented using a method

--- week-03/day-03/src/Checkerboard.java
+++ week-03/day-03/src/Checkerboard.java
@@ -9,11 +9,30 @@ public class Checkerboard {
     public static void mainDraw(Graphics graphics){
       // fill the canvas with a checkerboard pattern.
 
-
-
+      for (int i = 0; i < 8; i++) {
+        int iPass = i;
+        if (i % 2 == 0) {
+          drawRow(graphics,255, 0, iPass);
+        } else {
+          drawRow(graphics, 0, 255, iPass);
+        }
+      }
     }
 
-//    Don't touch the code below
+  private static void drawRow(Graphics graphics, int firstColor, int secondColor, int iterator) {
+    int squareSize = WIDTH / 8;
+    for (int j = 0; j < 8; j++) {
+      if (j % 2 == 0) {
+        graphics.setColor(new Color(firstColor, firstColor, firstColor));
+        graphics.fillRect(j * squareSize, iterator * squareSize, squareSize, squareSize);
+      } else {
+        graphics.setColor(new Color(secondColor, secondColor, secondColor));
+        graphics.fillRect(j * squareSize, iterator * squareSize, squareSize, squareSize);
+      }
+    }
+  }
+
+  //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
