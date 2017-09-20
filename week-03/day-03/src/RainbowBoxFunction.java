2951c504852bcdafc69db5d6f58351428581aad0
tomdreidel <redly.tamas@gmail.com>
2017-09-20 12:26:32 +0200

    working version with random generator method

--- week-03/day-03/src/RainbowBoxFunction.java
+++ week-03/day-03/src/RainbowBoxFunction.java
@@ -12,11 +12,23 @@ public class RainbowBoxFunction {
       // and draws a square of that size and color to the center of the canvas.
       // create a loop that fills the canvas with rainbow colored squares.
 
-
-
+      for (int i = 0; i < 20; i++) {
+        int squareSize = random8();
+        int[] color = {random8(), random8(), random8()};
+        dropSquare(graphics, squareSize, color);
+      }
     }
 
-//    Don't touch the code below
+  private static void dropSquare(Graphics graphics, int squareSize, int[] color) {
+    graphics.setColor(new Color(color[0], color[1], color[2]));
+    graphics.fillRect((WIDTH - squareSize) / 2, (HEIGHT - squareSize) / 2, squareSize, squareSize);
+  }
+
+  private static int random8() {
+    return (int) (Math.random() * 255);
+  }
+
+  //    Don't touch the code below
     static int WIDTH = 320;
     static int HEIGHT = 343;
 
