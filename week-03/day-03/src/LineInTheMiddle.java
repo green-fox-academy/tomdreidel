18cc8ad362f200812703e2662253ca5596b3a2b3
tomdreidel <redly.tamas@gmail.com>
2017-09-20 10:48:45 +0200

    exercise classes corrected

--- week-03/day-03/src/LineInTheMiddle.java
+++ week-03/day-03/src/LineInTheMiddle.java
@@ -4,11 +4,17 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Drawing {
+public class LineInTheMiddle {
 
     public static void mainDraw(Graphics graphics){
       //  draw a red horizontal line to the canvas' middle.
       //  draw a green vertical line to the canvas' middle.
+
+
+      graphics.setColor(Color.RED);
+      graphics.drawLine(0, HEIGHT / 2, WIDTH, HEIGHT / 2);
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(WIDTH / 2, 0 / 2, WIDTH / 2, HEIGHT);
 
 
 
