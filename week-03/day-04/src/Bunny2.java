1efab0486756ca57543d6e41fb09f00bcfad3706
tomdreidel <redly.tamas@gmail.com>
2017-09-21 12:10:24 +0200

    counting bunnys' ears + paws alike

--- week-03/day-04/src/Bunny2.java
+++ week-03/day-04/src/Bunny2.java
@@ -1,3 +1,26 @@
+public class Bunny2 {
+
+  public static void main(String[] args) {
+
+    int bunnyCount = 5;
+    System.out.println(earCursion(bunnyCount));
+  }
+
+  private static int earCursion(int bunnyCount) {
+    int earCounter = 0;
+
+    if (bunnyCount < 1) {
+      return earCounter;
+    }
+    if (bunnyCount % 2 == 0) {
+      earCounter += 3 + earCursion(bunnyCount - 1);
+      return earCounter;
+    }
+    earCounter += 2 + earCursion(bunnyCount - 1);
+    return earCounter;
+  }
+}
+
 // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
