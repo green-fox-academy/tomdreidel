0a9ee06abbbb076562a717432ff120cf2ec47cdb
tomdreidel <redly.tamas@gmail.com>
2017-09-21 12:44:14 +0200

    done this too

--- week-03/day-04/src/Refactorio.java
+++ week-03/day-04/src/Refactorio.java
@@ -1,2 +1,18 @@
+public class Refactorio {
+
+  public static void main(String[] args) {
+
+    int number = 6;
+    System.out.println(factorio(number));
+  }
+
+  private static int factorio(int number) {
+    if (number == 1) {
+      return 1;
+    }
+    return number * factorio(number - 1);
+  }
+}
+
 // - Create a recursive function called `refactorio`
 //   that returns it's input's factorial
