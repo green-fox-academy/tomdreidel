1d1646ab05d4fb5b92586dc2ab32ba148c03983a
tomdreidel <redly.tamas@gmail.com>
2017-09-21 10:56:42 +0200

    working counter

--- week-03/day-04/src/Counter.java
+++ week-03/day-04/src/Counter.java
@@ -1,1 +1,19 @@
+public class Counter {
+
+  public static void main(String[] args) {
+    int number = 20;
+    System.out.println(countdown(number));
+  }
+
+  private static String countdown(int number) {
+    String result = " " + number;
+    if (number == 1) {
+      result = result + " 0";
+      return result;
+    }
+    result = result + countdown(number - 1);
+    return result;
+  }
+}
+
 // Write a recursive function that takes one parameter: n and counts down from n.
