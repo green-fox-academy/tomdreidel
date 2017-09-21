50c8a1888b4d499047ca6f8e414a1887f0c0fe1e
tomdreidel <redly.tamas@gmail.com>
2017-09-21 12:59:54 +0200

    e*a*s*y *o*n*e

--- week-03/day-04/src/String3.java
+++ week-03/day-04/src/String3.java
@@ -1,2 +1,19 @@
+public class String3 {
+
+  public static void main(String[] args) {
+
+    String text = "given a string";
+    System.out.println(stringSeparator(text));
+  }
+
+  private static String stringSeparator(String text) {
+    String result = "";
+    if (text.length() == 0) {
+      return result;
+    }
+    return text.substring(0, 1) + "*" + stringSeparator(text.substring(1));
+  }
+}
+
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a "*".
