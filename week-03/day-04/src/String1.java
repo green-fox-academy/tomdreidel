179ef38e8971ce54116e02b0a17c96eb9c251aff
tomdreidel <redly.tamas@gmail.com>
2017-09-21 12:42:25 +0200

    xess! it is working

--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -1,2 +1,22 @@
+public class String1 {
+
+  public static void main(String[] args) {
+
+    String text = "xoga is xesokax for xou!";
+    System.out.println(wyeMachine(text));
+  }
+
+  private static String wyeMachine(String text) {
+    String result = "";
+    if (text.length() == 0) {
+      return result;
+    }
+    else if (text.substring(0, 1).equals("x")) {
+      return "y" + wyeMachine(text.substring(1));
+    }
+    return text.substring(0, 1) + wyeMachine(text.substring(1));
+  }
+}
+
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
