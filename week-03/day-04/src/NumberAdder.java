d1e178f81ae61c0fa18f1d2816c3523d22d1ebbf
tomdreidel <redly.tamas@gmail.com>
2017-09-21 11:01:19 +0200

    working solution

--- week-03/day-04/src/NumberAdder.java
+++ week-03/day-04/src/NumberAdder.java
@@ -1,1 +1,17 @@
+public class NumberAdder {
+
+  public static void main(String[] args) {
+
+    int number = 1000;
+    System.out.println(addNumbers(number));
+  }
+
+  private static int addNumbers(int number) {
+    if (number == 1) {
+      return 1;
+    }
+    return number + (number - 1);
+  }
+}
+
 // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
