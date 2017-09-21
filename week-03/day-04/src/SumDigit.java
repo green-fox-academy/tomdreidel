265abcacf581cf7344da29d280b74ef11788a7c0
tomdreidel <redly.tamas@gmail.com>
2017-09-21 11:50:40 +0200

    working recursive method

--- week-03/day-04/src/SumDigit.java
+++ week-03/day-04/src/SumDigit.java
@@ -1,3 +1,24 @@
-// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+public class SumDigit {
+
+  public static void main(String[] args) {
+
+    int number = 54321;
+    System.out.println(sumDigit(number));
+  }
+
+  private static int sumDigit(int number) {
+    int sum = number % 10;
+    if (number < 0) {
+      return 0;
+    } else if (number / 10 == 0) {
+      sum = number;
+      return sum;
+    }
+    sum += sumDigit(number / 10);
+    return sum;
+  }
+}
+
+// Given a non-negative int n, return the sum of its digits recursively (no loops).
 // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
 // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
