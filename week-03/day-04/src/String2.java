f86d98bfe698465d59bdc35a6a9ed26eade1623b
tomdreidel <redly.tamas@gmail.com>
2017-09-21 12:55:32 +0200

    alxso doxne

--- week-03/day-04/src/String2.java
+++ week-03/day-04/src/String2.java
@@ -1,1 +1,21 @@
+public class String2 {
+
+  public static void main(String[] args) {
+
+    String text = "Whxat ixs xwroxng wxith mxy kexyboxarxd?";
+    System.out.println(deXer(text));
+  }
+
+  private static String deXer(String text) {
+    String result = "";
+    if (text.length() == 0) {
+      return result;
+    }
+    else if (text.substring(0, 1).equals("x")) {
+      return deXer(text.substring(1));
+    }
+    return text.substring(0, 1) + deXer(text.substring(1));
+  }
+}
+
 // Given a string, compute recursively a new string where all the 'x' chars have been removed.
