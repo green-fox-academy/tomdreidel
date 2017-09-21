0b364b74e3cdffb31f354ac307e8eb03cfeea8e3
tomdreidel <redly.tamas@gmail.com>
2017-09-21 13:19:24 +0200

    I have reached the next fibo support

--- week-03/day-04/src/Fibonacci.java
+++ week-03/day-04/src/Fibonacci.java
@@ -1,3 +1,27 @@
+public class Fibonacci {
+
+  public static void main(String[] args) {
+
+    int number = 8;
+    System.out.println(fiboMachine(number));
+  }
+
+  private static int fiboMachine(int number) {
+    int fibonacci = 0;
+    if (number < 0) {
+      return 0;
+    }
+    else if (number == 0) {
+      return fibonacci;
+    }
+    else if (number == 1) {
+      return fibonacci + 1;
+    }
+    fibonacci += fiboMachine(number - 2) + fiboMachine(number - 1);
+    return fibonacci;
+  }
+}
+
 // The fibonacci sequence is a famous bit of mathematics, and it happens to
 // have a recursive definition. The first two values in the sequence are
 // 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the
