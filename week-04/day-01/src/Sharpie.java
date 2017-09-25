a05e7b295fcd8f81c41d2686ff84487b65d79a55
tomdreidel <redly.tamas@gmail.com>
2017-09-25 16:43:42 +0200

    sharpies gonna sharp

---
+++ week-04/day-01/src/Sharpie.java
@@ -0,0 +1,30 @@
+public class Sharpie {
+  //  Create Sharpie class
+  //  We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+  //  When creating one, we need to specify the color and the width
+  //  Every sharpie is created with a default 100 as inkAmount
+  //  We can use() the sharpie objects
+  //  which decreases inkAmount
+
+  String color;
+  float width;
+  float inkAmount;
+
+  public Sharpie(String color, float width) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = 100;
+  }
+
+  void use() {
+    inkAmount -= 5;
+  }
+
+  public static void main(String[] args) {
+
+    Sharpie blueSharpie = new Sharpie("blue", 10);
+    blueSharpie.use();
+    System.out.println(blueSharpie.color);
+    System.out.println(blueSharpie.inkAmount);
+  }
+}
