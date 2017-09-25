156e9965813252ea125069a2b819592460a43e36
tomdreidel <redly.tamas@gmail.com>
2017-09-25 15:10:09 +0200

    dominoes are in order

---
+++ week-04/day-01/src/Dominoes.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dominoes {
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    System.out.println(dominoes);
+
+    ArrayList<Domino> solution = new ArrayList<>();
+    solution.add(dominoes.get(0));
+
+    for (int i = 0; i < dominoes.size() - 1; i++) {
+      for (int j = 1; j < dominoes.size(); j++) {
+        if ((dominoes.get(j).getValues())[0] == solution.get(solution.size() - 1).getValues()[1]) {
+          solution.add(dominoes.get(j));
+        }
+      }
+    }
+
+    System.out.println(solution);
+  }
+
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
+}
+
+class Domino {
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+}
